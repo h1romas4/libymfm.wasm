91fc9abf153989e4737654bdfd0c77361154867d
h1romas4 <h1romas4@gmail.com>
2021-07-07 21:22:06 +0900

    add wasi_snapshot_preview1 function

--- examples/web/src/js/wasi_snapshot_preview1.js
+++ examples/web/src/js/wasi_snapshot_preview1.js
@@ -28,3 +28,51 @@ export const fd_write = function(fd, iovs, iovsLen, nwritten) {}; // eslint-disa
  * @param {number} fd
  */
 export const fd_close = function(fd) {}; // eslint-disable-line
+
+/**
+ * fd_fdstat_get
+ *
+ * @param {number} fd
+ * @param number} bufPtr
+ */
+export const fd_fdstat_get = function(fd, bufPtr) {}  // eslint-disable-line
+
+/**
+ * path_open
+ *
+ * @param {number} dirfd
+ * @param {number} dirflags
+ * @param {number} pathPtr
+ * @param {number} pathLen
+ * @param {number} oflags
+ * @param {BigIntPolyfillType | number} fsRightsBase
+ * @param {BigIntPolyfillType | number} fsRightsInheriting
+ * @param {number} fsFlags
+ * @param {number} fd
+ */
+export const path_open = function (dirfd, dirflags, pathPtr, pathLen, oflags, fsRightsBase, fsRightsInheriting, fsFlags, fd) {}  // eslint-disable-line
+
+/**
+ * fd_fdstat_set_flags
+ *
+ * @param {number} dirfd
+ * @param {number} dirflags
+ * @param {number} pathPtr
+ * @param {number} pathLen
+ * @param {number} oflags
+ * @param {BigIntPolyfillType | number} fsRightsBase
+ * @param {BigIntPolyfillType | number} fsRightsInheriting
+ * @param {number} fsFlags
+ * @param {number} fd
+ */
+export const fd_fdstat_set_flags = function(dirfd, dirflags, pathPtr, pathLen, oflags, fsRightsBase, fsRightsInheriting, fsFlags, fd) {}  // eslint-disable-line
+
+/**
+ * fd_read
+ *
+ * @param {number} fd
+ * @param {number} iovs
+ * @param {number} iovsLen
+ * @param {number} nread
+ */
+export const fd_read = function(fd, iovs, iovsLen, nread) {}  // eslint-disable-line
