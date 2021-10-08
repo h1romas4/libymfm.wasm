546c9128081065b5752b60d4c4df885198681f23
h1romas4 <h1romas4@gmail.com>
2021-10-08 21:02:51 +0900

    fix LinearUpSamplingStream

--- src/rust/sound/stream.rs
+++ src/rust/sound/stream.rs
@@ -101,13 +101,12 @@ impl SoundStream for NearestDownSampleStream {
 /// Resample stream for sound chip
 ///
 pub struct LinearUpSamplingStream {
-    now_input_sampling_l: f32,
-    now_input_sampling_r: f32,
-    prev_input_sampling_l: f32,
-    prev_input_sampling_r: f32,
+    now_input_sampling_l: Option<f32>,
+    now_input_sampling_r: Option<f32>,
+    prev_input_sampling_l: Option<f32>,
+    prev_input_sampling_r: Option<f32>,
     output_sampling_pos: f64,
     output_sampling_step: f64,
-    output_sampling_inv: f64,
     output_sampling_l: f32,
     output_sampling_r: f32,
 }
@@ -117,16 +116,22 @@ impl LinearUpSamplingStream {
         assert!(input_sampling_rate <= output_sampling_rate);
         let output_sampling_step = input_sampling_rate as f64 / output_sampling_rate as f64;
         LinearUpSamplingStream {
-            now_input_sampling_l: 0_f32,
-            now_input_sampling_r: 0_f32,
-            prev_input_sampling_l: 0_f32,
-            prev_input_sampling_r: 0_f32,
-            output_sampling_pos: 0_f64,
+            now_input_sampling_l: None,
+            now_input_sampling_r: None,
+            prev_input_sampling_l: None,
+            prev_input_sampling_r: None,
+            output_sampling_pos: 1_f64,
             output_sampling_step,
-            output_sampling_inv: 1_f64 / output_sampling_step,
             output_sampling_l: 0_f32,
             output_sampling_r: 0_f32,
         }
+    }
+
+    fn calc_sample(&self, prev_sample: f32, now_sample: f32) -> f32 {
+        let output_pos = self.output_sampling_pos + self.output_sampling_step;
+        (prev_sample as f64
+            + (output_pos - self.output_sampling_step as f64)
+                * (now_sample as f64 - prev_sample as f64)) as f32
     }
 }
 
@@ -136,23 +141,31 @@ impl SoundStream for LinearUpSamplingStream {
     /// to determine if it needs to be ticked.
     ///
     fn is_tick(&mut self) -> Tick {
-        if self.output_sampling_pos < 1_f64 {
-            self.output_sampling_l = (self.output_sampling_inv
-                * (self.prev_input_sampling_l as f64
-                    * (self.output_sampling_step - self.output_sampling_pos)
-                    + self.output_sampling_pos * self.now_input_sampling_l as f64))
-                as f32;
-            self.output_sampling_r = (self.output_sampling_inv
-                * (self.prev_input_sampling_r as f64
-                    * (self.output_sampling_step - self.output_sampling_pos)
-                    + self.output_sampling_pos * self.now_input_sampling_r as f64))
-                as f32;
-            return Tick::No;
+        if self.output_sampling_pos >= 1_f64 {
+            if self.now_input_sampling_l != None {
+                self.output_sampling_l = self.now_input_sampling_l.unwrap();
+                self.output_sampling_r = self.now_input_sampling_r.unwrap();
+            }
+            self.output_sampling_pos -= 1_f64;
+            Tick::One
+        } else {
+            // No sound is produced until the previous sample is confirmed.
+            if self.prev_input_sampling_l == None {
+                self.output_sampling_l = 0_f32;
+                self.output_sampling_r = 0_f32;
+            } else {
+                // y(t/Ts) = x[n] + (t/Ts-n)(x[n+1]-x[n]);
+                self.output_sampling_l = self.calc_sample(
+                    self.prev_input_sampling_l.unwrap(),
+                    self.now_input_sampling_l.unwrap(),
+                );
+                self.output_sampling_r = self.calc_sample(
+                    self.prev_input_sampling_r.unwrap(),
+                    self.now_input_sampling_r.unwrap(),
+                );
+            }
+            Tick::No
         }
-        self.output_sampling_l = self.prev_input_sampling_l;
-        self.output_sampling_r = self.prev_input_sampling_r;
-        self.output_sampling_pos -= 1_f64;
-        Tick::One
     }
 
     ///
@@ -161,8 +174,8 @@ impl SoundStream for LinearUpSamplingStream {
     fn push(&mut self, sampling_l: f32, sampling_r: f32) {
         self.prev_input_sampling_l = self.now_input_sampling_l;
         self.prev_input_sampling_r = self.now_input_sampling_r;
-        self.now_input_sampling_l = sampling_l;
-        self.now_input_sampling_r = sampling_r;
+        self.now_input_sampling_l = Some(sampling_l);
+        self.now_input_sampling_r = Some(sampling_r);
     }
 
     ///
@@ -202,10 +215,83 @@ pub fn convert_sample_i2f(i32_sample: i32) -> f32 {
 
 #[cfg(test)]
 mod tests {
+    #[allow(unused_imports)]
+    use approx_eq::assert_approx_eq;
+
     use crate::sound::stream::LinearUpSamplingStream;
+    use crate::sound::stream::Tick;
+
+    use super::SoundStream;
 
     #[test]
     fn make_stream_1() {
-        let _stream = LinearUpSamplingStream::new(44100, 44100);
+        let mut stream = LinearUpSamplingStream::new(1, 4);
+
+        println!("= step 0 (0.0, 0.0) -> (1.0, 1.0)");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(1_f32, 1_f32);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+
+        println!("\n= step 1 (1.0, 1.0) -> (1.0, 1.0)");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(1_f32, 1_f32);
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+
+        println!("\n= step 2 (1.0, 1.0) -> (0.0, 0.0)");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(0_f32, 0_f32);
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0.75_f32, 0.75_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0.5_f32, 0.5_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0.25_f32, 0.25_f32));
+
+        println!("\n= step 3 (0.0, 0.0) -> (-1.0, -1.0)");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(-1_f32, -1_f32);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (-0.25_f32, -0.25_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (-0.5_f32, -0.5_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (-0.75_f32, -0.75_f32));
+
+        println!("\n= step 4 (-1.0, -1.0) -> (1.0, 1.0)");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(1_f32, 1_f32);
+        assert_sampling(stream.drain(), (-1_f32, -1_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (-0.50_f32, -0.5_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0_f32, 0_f32));
+        assert!(stream.is_tick() == Tick::No);
+        assert_sampling(stream.drain(), (0.5_f32, 0.5_f32));
+        println!("@");
+        assert!(stream.is_tick() == Tick::One);
+        stream.push(2_f32, 2_f32); // dummy
+        assert_sampling(stream.drain(), (1_f32, 1_f32));
+    }
+
+    fn assert_sampling((l, r): (f32, f32), (tl, tr): (f32, f32)) {
+        println!("({}, {}) ({}, {})", l, r, tl, tr);
+        // #[allow(clippy::float_cmp)]
+        // assert_approx_eq!(l as f64, tl as f64);
+        // #[allow(clippy::float_cmp)]
+        // assert_approx_eq!(r as f64, tr as f64);
     }
 }
