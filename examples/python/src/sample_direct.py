7c47f69eefbf9717484df39e6a2652cadadb55d3
h1romas4 <h1romas4@gmail.com>
2021-11-13 22:13:06 +0900

    fix examle

--- examples/python/src/sample_direct.py
+++ examples/python/src/sample_direct.py
@@ -103,75 +103,81 @@ loop_count = 30
 # Play loop (1 loop is 1 tick)
 while loop_count > 0:
     #
-    # Analyze music sequence
+    # If the buffer advance is within 2, create a sampling data
     #
-    for track in range(0, 3):
-        if len(MUSIC_SEQUENCE[track]) <= 0:
-            continue
-        # Wait 1 tick
-        if wait[track] > 0:
-            wait[track] -= 1
-            continue
-        # Control commands (no wait)
-        while True:
-            # Get sequence command
-            command = MUSIC_SEQUENCE[track][seq_index[track]]
-            if command == 200:
-                # Set volume
+    if len(sampling_buffer) < 2:
+        #
+        # Analyze music sequence
+        #
+        for track in range(0, 3):
+            if len(MUSIC_SEQUENCE[track]) <= 0:
+                continue
+            # Wait 1 tick
+            if wait[track] > 0:
+                wait[track] -= 1
+                continue
+            # Control commands (no wait)
+            while True:
+                # Get sequence command
+                command = MUSIC_SEQUENCE[track][seq_index[track]]
+                if command == 200:
+                    # Set volume
+                    seq_index[track] += 1
+                    volume = MUSIC_SEQUENCE[track][seq_index[track]]
+                    chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track + 0x8, volume)
+                elif command == 201:
+                    # Mixing control
+                    seq_index[track] += 1
+                    data = MUSIC_SEQUENCE[track][seq_index[track]]
+                    tone = (data & 0b10) << track + 2
+                    noise = (data & 0b01) << track
+                    mixing = mixing | (tone | noise)
+                    chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, 0x7, mixing)
+                elif command == 210:
+                    # Detune
+                    seq_index[track] += 1
+                    detune[track] = MUSIC_SEQUENCE[track][seq_index[track]]
+                else:
+                    break
+                # Next command
                 seq_index[track] += 1
-                volume = MUSIC_SEQUENCE[track][seq_index[track]]
-                chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track + 0x8, volume)
-            elif command == 201:
-                # Mixing control
-                seq_index[track] += 1
-                data = MUSIC_SEQUENCE[track][seq_index[track]]
-                tone = (data & 0b10) << track + 2
-                noise = (data & 0b01) << track
-                mixing = mixing | (tone | noise)
-                chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, 0x7, mixing)
-            elif command == 210:
+            # Tone
+            if 0 <= command < len(TONE_TABLE):
+                tone = TONE_TABLE[command]
+                # 16bit little endian
+                lower = tone & 0xff
+                upper = (tone & 0xff00) >> 8
                 # Detune
+                lower += detune[track]
+                # Write YM2149
+                # Set frequency
+                chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track * 2, lower)
+                chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track * 2 + 1, upper)
+                # Set wait tick
                 seq_index[track] += 1
-                detune[track] = MUSIC_SEQUENCE[track][seq_index[track]]
-            else:
-                break
-            # Next command
+                wait[track] = MUSIC_SEQUENCE[track][seq_index[track]]
+            # Next sequence
             seq_index[track] += 1
-        # Tone
-        if 0 <= command < len(TONE_TABLE):
-            tone = TONE_TABLE[command]
-            # 16bit little endian
-            lower = tone & 0xff
-            upper = (tone & 0xff00) >> 8
-            # Detune
-            lower += detune[track]
-            # Write YM2149
-            # Set frequency
-            chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track * 2, lower)
-            chip_stream.sound_slot_write(SOUND_SLOT_INDEX, SoundChipType.YM2149, 0, track * 2 + 1, upper)
-            # Set wait tick
-            seq_index[track] += 1
-            wait[track] = MUSIC_SEQUENCE[track][seq_index[track]]
-        # Next sequence
-        seq_index[track] += 1
-        # Loop sequence
-        if seq_index[track] >= len(MUSIC_SEQUENCE[track]):
-            seq_index[track] = 0
-            loop_count -= 1
+            # Loop sequence
+            if seq_index[track] >= len(MUSIC_SEQUENCE[track]):
+                seq_index[track] = 0
+                loop_count -= 1
+        #
+        # Update sound
+        #
+        # Tick "One" sound slot (SOUND_DRIVER_TICK_RATE = 1/60)
+        chip_stream.sound_slot_update(SOUND_SLOT_INDEX, 1)
+        # Query sampling chunk filled
+        if chip_stream.sound_slot_is_stream_filled(SOUND_SLOT_INDEX) == 1:
+            # Stream sound slot
+            chip_stream.sound_slot_stream(SOUND_SLOT_INDEX)
+            # Get sampling referance
+            s16le = chip_stream.sound_slot_get_sampling_ref(SOUND_SLOT_INDEX)
+            # Add to the sampling buffer
+            sampling_buffer.append(s16le)
     #
-    # Update sound
-    #
-    # Tick "One" sound slot (SOUND_DRIVER_TICK_RATE = 1/60)
-    chip_stream.sound_slot_update(SOUND_SLOT_INDEX, 1)
-    # Query sampling chunk filled
-    if chip_stream.sound_slot_is_stream_filled(SOUND_SLOT_INDEX) == 1:
-        # Stream sound slot
-        chip_stream.sound_slot_stream(SOUND_SLOT_INDEX)
-        # Get sampling referance
-        s16le = chip_stream.sound_slot_get_sampling_ref(SOUND_SLOT_INDEX)
-        # Add to the sampling buffer
-        sampling_buffer.append(s16le)
     # If the sampling buffer is full, the sound will be played
+    #
     if len(sampling_buffer) > 0 and pygame.mixer.get_busy() == False:
         # Remove the first element from the sampling buffer and pack it
         s16le = pygame.mixer.Sound(buffer=sampling_buffer.pop(0))
